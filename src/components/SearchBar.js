25cede92949f3f71b1a7b1021b313413ba992b59
ivanfslee <ivanfslee@gmail.com>
2019-12-02 14:02:48 -0800

    Added axios youtubeAPI request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = event => {
+        // console.log(event.target.value);
+        this.setState({ term: event.target.value })
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+       //TODO: call callback func from parent compo, to tell parent user submitted a term and that parent should initiate ajax requext to youtubeAPI
+       this.props.onFormSubmit(this.state.term);
+    };
+
     render() {
-        return <div>Search Bar</div>
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input onChange={this.onInputChange} type="text" value={this.state.term}/>
+                    </div>
+                </form>
+            </div>
+        ); 
     }
 }
 
